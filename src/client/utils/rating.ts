c674c6f405fbf6fbabfa5bb2a706f6e4a58f6d96
Caxer <webmaster@caxerx.com>
2022-05-23 09:03:42 +0800

    fix: rating calculation

--- src/client/utils/rating.ts
+++ src/client/utils/rating.ts
@@ -1,42 +1,97 @@
 import BigNumber from 'bignumber.js';
 
 export function calculateRating(score: BigNumber, songRating: BigNumber) {
-  let offset = new BigNumber(0);
+  if (score.isLessThanOrEqualTo(500000)) return new BigNumber(0);
 
-  if (score.isGreaterThanOrEqualTo(1009000)) {
-    offset = new BigNumber(2.15);
-  } else if (score.isGreaterThanOrEqualTo(1007500)) {
-    offset = new BigNumber(2).plus(
-      score.minus(1007500).multipliedBy(5).dividedBy(50000),
-    );
-  } else if (score.isGreaterThanOrEqualTo(1005000)) {
-    offset = new BigNumber(1.5).plus(
-      score.minus(1005000).multipliedBy(10).dividedBy(50000),
-    );
-  } else if (score.isGreaterThanOrEqualTo(1000000)) {
-    offset = new BigNumber(1).plus(
-      score.minus(1000000).multipliedBy(5).dividedBy(50000),
-    );
-  } else if (score.isGreaterThanOrEqualTo(975000)) {
-    offset = score.minus(975000).multipliedBy(2).dividedBy(50000);
-  } else if (score.isGreaterThanOrEqualTo(950000)) {
-    offset = new BigNumber(-1.5).plus(
-      score.minus(950000).multipliedBy(3).dividedBy(50000),
-    );
-  } else if (score.isGreaterThanOrEqualTo(925000)) {
-    offset = new BigNumber(-3).plus(
-      score.minus(925000).multipliedBy(3).dividedBy(50000),
-    );
-  } else {
-    offset = new BigNumber(-5).plus(
-      score.minus(900000).multipliedBy(4).dividedBy(50000),
-    );
+  if (score.isLessThanOrEqualTo(800000)) {
+    return BigNumber.max(songRating.minus(5).dividedBy(2), 0)
+      .multipliedBy(score.minus(500000).dividedBy(300000))
+      .decimalPlaces(2, BigNumber.ROUND_FLOOR);
   }
-  return songRating
-    .plus(offset)
-    .multipliedBy(100)
-    .decimalPlaces(0, BigNumber.ROUND_FLOOR)
-    .dividedBy(100);
+
+  if (score.isLessThanOrEqualTo(900000))
+    return BigNumber.max(songRating.minus(5).dividedBy(2), 0)
+      .plus(
+        BigNumber.max(
+          songRating
+            .minus(5)
+            .minus(BigNumber.max(songRating.minus(5).dividedBy(2), 0)),
+          0,
+        ).multipliedBy(score.minus(800000).dividedBy(100000)),
+      )
+      .decimalPlaces(2, BigNumber.ROUND_FLOOR);
+
+  if (score.isLessThanOrEqualTo(900000))
+    return BigNumber.max(songRating.minus(5).dividedBy(2), 0)
+      .plus(
+        BigNumber.min(songRating, 2).multipliedBy(
+          score.minus(900000).dividedBy(25000),
+        ),
+      )
+      .decimalPlaces(2, BigNumber.ROUND_FLOOR);
+
+  if (score.isLessThanOrEqualTo(925000))
+    return BigNumber.max(songRating.minus(5), 0)
+      .plus(
+        BigNumber.min(songRating, 2).multipliedBy(
+          score.minus(900000).dividedBy(25000),
+        ),
+      )
+      .decimalPlaces(2, BigNumber.ROUND_FLOOR);
+
+  if (score.isLessThanOrEqualTo(975000))
+    return BigNumber.max(songRating.minus(3), 0)
+      .plus(
+        BigNumber.min(songRating, 3).multipliedBy(
+          score.minus(925000).dividedBy(50000),
+        ),
+      )
+      .decimalPlaces(2, BigNumber.ROUND_FLOOR);
+
+  if (score.isLessThanOrEqualTo(1000000))
+    return songRating.plus(
+      score
+        .minus(975000)
+        .dividedBy(250)
+        .decimalPlaces(0, BigNumber.ROUND_FLOOR)
+        .multipliedBy(0.01),
+    );
+
+  if (score.isLessThanOrEqualTo(1005000))
+    return songRating
+      .plus(1)
+      .plus(
+        score
+          .minus(1000000)
+          .dividedBy(100)
+          .decimalPlaces(0, BigNumber.ROUND_FLOOR)
+          .multipliedBy(0.01),
+      );
+
+  if (score.isLessThanOrEqualTo(1007500))
+    return songRating
+      .plus(1.5)
+      .plus(
+        score
+          .minus(1005000)
+          .dividedBy(50)
+          .decimalPlaces(0, BigNumber.ROUND_FLOOR)
+          .multipliedBy(0.01),
+      );
+
+  if (score.isLessThanOrEqualTo(1009000)) {
+    return songRating
+      .plus(2)
+      .plus(
+        score
+          .minus(1007500)
+          .dividedBy(100)
+          .decimalPlaces(0, BigNumber.ROUND_FLOOR)
+          .multipliedBy(0.01),
+      );
+  }
+
+  return songRating.plus(2.15);
 }
 
 export function calculateBest30(ratingList: BigNumber[]) {
